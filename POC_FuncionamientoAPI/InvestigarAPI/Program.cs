fb724b32ac51e8d6e1b1771528760cb58bc78807
José Ignacio Zurbano <joseignaciozurbano@gmail.com>
2023-07-21 13:18:42 +0200

    Acaba la prueba de la API

--- POC_FuncionamientoAPI/InvestigarAPI/Program.cs
+++ POC_FuncionamientoAPI/InvestigarAPI/Program.cs
@@ -2,19 +2,18 @@
 
 namespace InvestigarAPI
 {
-    public class Persona
-    {
-        public string Base_code { get; set; }
-    }
-
     class Program
     {
         static async Task Main(string[] args)
         {
             string apiUrl = "https://v6.exchangerate-api.com/v6/a2add358f81f3345c05f21c9/latest/USD";
+            string apiKey = "a2add358f81f3345c05f21c9";
 
             using (var httpClient = new HttpClient())
             {
+                // Agregar la clave de acceso en los headers de la solicitud
+                httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+
                 try
                 {
                     // Realizar la solicitud GET a la API y obtener la respuesta
@@ -23,18 +22,28 @@ namespace InvestigarAPI
                     // Verificar si la solicitud fue exitosa
                     response.EnsureSuccessStatusCode();
 
-                    // Leer el contenido de la respuesta como una cadena JSON
+                    // Leer el contenido de la respuesta como una cadena JSON u otro formato según la API
                     string jsonResponse = await response.Content.ReadAsStringAsync();
 
-                    Console.WriteLine(jsonResponse);
 
-                    //Deserializar la cadena JSON en una lista de objetos Persona
-                    var personas = JsonConvert.DeserializeObject<List<Persona>>(jsonResponse);
+                    // Convertir archivo json a ResultadoJson
+                    ResultadoJson resulatadoAPI = JsonConvert.DeserializeObject<ResultadoJson>(jsonResponse);
 
-                    // Mostrar los resultados en la consola
-                    foreach (var persona in personas)
+
+                    // Mostrar los datos en la consola para verificar que se haya deserializado correctamente
+                    Console.WriteLine("Result: " + resulatadoAPI.Result);
+                    Console.WriteLine("Documentation: " + resulatadoAPI.Documentation);
+                    Console.WriteLine("Terms_Of_Use: " + resulatadoAPI.Terms_Of_Use);
+                    Console.WriteLine("Time_Last_Update_Unix: " + resulatadoAPI.Time_Last_Update_Unix);
+                    Console.WriteLine("Time_Last_Update_Utc: " + resulatadoAPI.Time_Last_Update_Utc);
+                    Console.WriteLine("Time_Next_Update_Unix: " + resulatadoAPI.Time_Next_Update_Unix);
+                    Console.WriteLine("Time_Next_Update_Utc: " + resulatadoAPI.Time_Next_Update_Utc);
+                    Console.WriteLine("Base_Code: " + resulatadoAPI.Base_Code);
+
+                    // Mostrar las tasas de conversión
+                    foreach (var conversionRate in resulatadoAPI.Conversion_Rates)
                     {
-                        Console.WriteLine($"Nombre: {persona.Base_code}");
+                        Console.WriteLine($"{conversionRate.Key}: {conversionRate.Value}");
                     }
                 }
                 catch (HttpRequestException ex)
@@ -53,4 +62,16 @@ namespace InvestigarAPI
         }
     }
 
+    public class ResultadoJson
+    {
+        public string Result { get; set; }
+        public string Documentation { get; set; }
+        public string Terms_Of_Use { get; set; }
+        public long Time_Last_Update_Unix { get; set; }
+        public string Time_Last_Update_Utc { get; set; }
+        public long Time_Next_Update_Unix { get; set; }
+        public string Time_Next_Update_Utc { get; set; }
+        public string Base_Code { get; set; }
+        public Dictionary<string, decimal> Conversion_Rates { get; set; }
+    }
 }
