7dc13396b27e7fa78f570efeec34376e9c309028
KarolisTamosa <ktamasi-ext@itracasa.es>
2023-07-21 09:08:19 +0200

    POC del menú

---
+++ POC_Menú_de_opciones_Solution/POC_Menú_de_opciones/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace POC_Menú_de_opciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("                                                                          ");
+                Console.WriteLine("                                                                          ");
+                Console.WriteLine("░█████╗░░█████╗░███╗░░██╗██╗░░░██╗███████╗██████╗░░██████╗░█████╗░██████╗░");
+                Console.WriteLine("██╔══██╗██╔══██╗████╗░██║██║░░░██║██╔════╝██╔══██╗██╔════╝██╔══██╗██╔══██╗");
+                Console.WriteLine("██║░░╚═╝██║░░██║██╔██╗██║╚██╗░██╔╝█████╗░░██████╔╝╚█████╗░██║░░██║██████╔╝");
+                Console.WriteLine("██║░░██╗██║░░██║██║╚████║░╚████╔╝░██╔══╝░░██╔══██╗░╚═══██╗██║░░██║██╔══██╗");
+                Console.WriteLine("╚█████╔╝╚█████╔╝██║░╚███║░░╚██╔╝░░███████╗██║░░██║██████╔╝╚█████╔╝██║░░██║");
+                Console.WriteLine("░╚════╝░░╚════╝░╚═╝░░╚══╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝");
+                Console.WriteLine("                                                                          ");
+                Console.WriteLine("╔════════════════════════════════════════════════════════════════════════╗");
+                Console.WriteLine("║   1. Conversor de monedas                                              ║");
+                Console.WriteLine("║   2. Listado de divisas                                                ║");
+                Console.WriteLine("║   3. Editor de divisas                                                 ║");
+                Console.WriteLine("║   4. Historial de conversiones                                         ║");
+                Console.WriteLine("║   5. Salir                                                             ║");
+                Console.WriteLine("╚════════════════════════════════════════════════════════════════════════╝");
+                Console.WriteLine("                         Powered by LevelUp_Team                          ");
+                Console.WriteLine("                                                                          ");
+                Console.WriteLine("          Escribe el número de la opción que deseas realizar.             ");
+                Console.ReadLine();
+            }
+        }
+    }
+}
