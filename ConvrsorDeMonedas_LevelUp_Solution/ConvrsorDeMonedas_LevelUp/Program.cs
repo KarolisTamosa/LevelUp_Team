e0fc315ae644d66d6eb23872f172cb77fed6b6c1
KarolisTamosa <ktamasi-ext@itracasa.es>
2023-07-21 13:09:23 +0200

    Update. Menú terminado

--- ConvrsorDeMonedas_LevelUp_Solution/ConvrsorDeMonedas_LevelUp/Program.cs
+++ ConvrsorDeMonedas_LevelUp_Solution/ConvrsorDeMonedas_LevelUp/Program.cs
@@ -1,10 +1,31 @@
 ﻿namespace ConvrsorDeMonedas_LevelUp
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            {
+                Console.WriteLine("                                                                          ");
+                Console.WriteLine("                                                                          ");
+                Console.WriteLine("░█████╗░░█████╗░███╗░░██╗██╗░░░██╗███████╗██████╗░░██████╗░█████╗░██████╗░");
+                Console.WriteLine("██╔══██╗██╔══██╗████╗░██║██║░░░██║██╔════╝██╔══██╗██╔════╝██╔══██╗██╔══██╗");
+                Console.WriteLine("██║░░╚═╝██║░░██║██╔██╗██║╚██╗░██╔╝█████╗░░██████╔╝╚█████╗░██║░░██║██████╔╝");
+                Console.WriteLine("██║░░██╗██║░░██║██║╚████║░╚████╔╝░██╔══╝░░██╔══██╗░╚═══██╗██║░░██║██╔══██╗");
+                Console.WriteLine("╚█████╔╝╚█████╔╝██║░╚███║░░╚██╔╝░░███████╗██║░░██║██████╔╝╚█████╔╝██║░░██║");
+                Console.WriteLine("░╚════╝░░╚════╝░╚═╝░░╚══╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝");
+                Console.WriteLine("                                                                          ");
+                Console.WriteLine("╔════════════════════════════════════════════════════════════════════════╗");
+                Console.WriteLine("║   1. Conversor de monedas                                              ║");
+                Console.WriteLine("║   2. Listado de divisas                                                ║");
+                Console.WriteLine("║   3. Editor de divisas                                                 ║");
+                Console.WriteLine("║   4. Historial de conversiones                                         ║");
+                Console.WriteLine("║   5. Salir                                                             ║");
+                Console.WriteLine("╚════════════════════════════════════════════════════════════════════════╝");
+                Console.WriteLine("                         Powered by LevelUp_Team                          ");
+                Console.WriteLine("                                                                          ");
+                Console.WriteLine("          Escribe el número de la opción que deseas realizar.             ");
+                Console.ReadLine();
+            }
         }
     }
 }
