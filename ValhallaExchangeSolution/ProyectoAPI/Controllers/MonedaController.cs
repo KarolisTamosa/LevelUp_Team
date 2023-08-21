375d24710813dfd04d756a05ea0450931a065a46
chrispinday <chrispinday@gmail.com>
2023-08-21 14:04:36 +0200

    Post de conversor hecho

--- ValhallaExchangeSolution/ProyectoAPI/Controllers/MonedaController.cs
+++ ValhallaExchangeSolution/ProyectoAPI/Controllers/MonedaController.cs
@@ -1,5 +1,6 @@
 ﻿using Domain.IServices;
 using Domain.Models;
+using DTO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -41,6 +42,30 @@ namespace ProyectoAPI.Controllers
                 return BadRequest(ex + " - ERROR NUESTRO");//400
             }
             
+        }
+        [Route("ConvertirMoneda")]
+        [HttpPost]
+        public async Task<ActionResult<double>> Convertir([FromBody] ConversorDTO conversorDTO)
+        {
+            try
+            {
+                string codigoMonedaOrigen = conversorDTO.CodigoMonedaOrigen;
+                string codigoMonedaDestino = conversorDTO.CodigoMonedaDestino;
+                double importe = conversorDTO.Importe;
+                //verificar si existe moneda
+                var monedaOrigen = await _monedaService.ObtenerMonedaPorCodigo(codigoMonedaOrigen);
+                var monedaDestino = await _monedaService.ObtenerMonedaPorCodigo(codigoMonedaDestino);
+                if (monedaOrigen == null || monedaDestino == null|| importe < 0)
+                {
+                    return BadRequest("Alguno de los codigos de las monedas introducidas no son validas O el importe introduciodo es negativo");
+                }
+                var resultado = _monedaService.ObtenerResultadoConvertirMoneda(monedaOrigen, monedaDestino, importe);
+                return Ok(resultado);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
