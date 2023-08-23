27288d7d3c79f17bd2e45d762cfc1a6ea41b3eba
Karolis <ktamosaxlv@gmail.com>
2023-08-23 12:37:07 +0200

    Integración de paises

---
+++ ValhallaExchangeSolution/Domain/IRepositories/PaisRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.IRepositories
+{
+    public interface IPaisRepository
+    {
+        Task<IEnumerable<Pais>> GetPais();
+        Task MeterPaises(List<Pais> Paises);
+        Task<Moneda> ObtenerPaisPorId(Guid IdPais);
+    }
+}
---
+++ ValhallaExchangeSolution/Domain/IServices/IPaisService.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.IServices
+{
+    public interface IPaisService
+    {
+        Task<IEnumerable<Pais>> GetPaisPorUsuario(Guid IdPais);
+    }
+}
