20db895c88a59c86230b3c382849891331932698
chrispinday <chrispinday@gmail.com>
2023-08-28 13:30:03 +0200

    migraciones exitosas con datos demo

---
+++ ValhallaExchangeSolution/Persistence/Migrations/20230828102735_enblanco2.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class enblanco2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"--Insertar procedimientos almacenados
+CREATE PROCEDURE GetHistorialFromUsuarios
+
+    @UsuarioId UNIQUEIDENTIFIER,
+
+    @NumResultados INT
+
+AS
+
+BEGIN
+
+    SELECT TOP (@NumResultados) *
+
+    FROM Historial
+
+    WHERE IdUsuario = @UsuarioId AND Eliminado = 0
+
+    ORDER BY FechaConversion DESC
+
+END;");
+
+            
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
