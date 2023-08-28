02468d1b186eb488294eada4aa45639a2c85c62b
Karolis <ktamosaxlv@gmail.com>
2023-08-28 13:58:41 +0200

    Ya funciona El proyecto Angular con las Apis

--- ValhallaExchangeSolution/ProyectoAPI/Program.cs
+++ ValhallaExchangeSolution/ProyectoAPI/Program.cs
@@ -2,12 +2,11 @@ using AutoMapper;
 using Domain.IRepositories;
 using Domain.IServices;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json.Serialization;
 using Persistence.Context;
 using Persistence.Repositories;
 using Services;
-
-
 
 namespace ProyectoAPI
 {
@@ -17,37 +16,30 @@ namespace ProyectoAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProyectoAPI", Version = "v1" });
+            });
 
-
-            // Add services to the container.
-
-
-
-            //Contexto de base de datos
+            // Contexto de base de datos
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
             {
                 options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=PruebaBD3;Trusted_Connection=True;MultipleActiveResultSets=true");
-            }
-            );
+            });
 
-
-
-            //Servicios
+            // Servicios
             builder.Services.AddScoped<IMonedaService, MonedaService>();
             builder.Services.AddScoped<IApiMonedasService, ApiMonedasService>();
             builder.Services.AddScoped<IHistorialService, HistorialService>();
             builder.Services.AddScoped<IUsuarioService, UsuarioService>();
             builder.Services.AddScoped<IPaisService, PaisService>();
 
-            //Repositorios
+            // Repositorios
             builder.Services.AddScoped<IMonedaRepository, MonedaRepository>();
             builder.Services.AddScoped<IHistorialRepository, HistorialRepository>();
             builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
             builder.Services.AddScoped<IPaisRepository, PaisRepository>();
-
-
-
-
 
             builder.Services.AddControllers().AddNewtonsoftJson(setupAction =>
             {
@@ -55,42 +47,38 @@ namespace ProyectoAPI
                     new CamelCasePropertyNamesContractResolver();
             });
 
-
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            // Mapper
+            // Añadir AutoMapper
             builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-
+            // Configurar CORS
+            builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200")
+                           .AllowAnyHeader()
+                           .AllowAnyMethod();
+                });
+            });
 
             var app = builder.Build();
 
-
-
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProyectoAPI v1");
+                });
             }
-
-
 
             app.UseHttpsRedirection();
 
-
-
             app.UseAuthorization();
 
-
-
-
+            app.UseCors();
 
             app.MapControllers();
-
-
 
             app.Run();
         }
